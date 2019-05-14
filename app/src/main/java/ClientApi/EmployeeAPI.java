5e676f0c13333df11d1e67e64ea7a8708a5c8836
anupant01 <pantaanupa@gmail.com>
2019-05-14 11:49:02 +0545

    Register done

--- app/src/main/java/ClientApi/EmployeeAPI.java
+++ app/src/main/java/ClientApi/EmployeeAPI.java
@@ -3,8 +3,11 @@ package ClientApi;
 import java.util.List;
 
 import model.Employee;
+import model.EmployeeCUD;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
@@ -16,4 +19,8 @@ public interface EmployeeAPI {
     //to get employee on the basis of their id
     @GET("employee/{empID}")
     Call<Employee> getEmployeeByID(@Path("empID") int empID);
+
+    //to create or register
+    @POST("create")
+    Call<Void> registerEmployee (@Body EmployeeCUD emp);
 }
--- app/src/main/java/com/e/api/RegistrationActivity.java
+++ app/src/main/java/com/e/api/RegistrationActivity.java
@@ -2,12 +2,72 @@ package com.e.api;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ClientApi.EmployeeAPI;
+import model.Employee;
+import model.EmployeeCUD;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private final static String BASEURL ="http://dummy.restapiexample.com/api/v1/";
+    private EditText etName, etSalary,etAge;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        etSalary = findViewById(R. id.etSalary);
+        btnRegister = findViewById(R.id.btnReg);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+
+    }
+
+    private void Register(){
+        String name = etName.getText().toString();
+        Float salary = Float.parseFloat(etSalary.getText().toString());
+        int age = Integer.parseInt(etAge.getText().toString());
+
+        EmployeeCUD employeeCUD = new EmployeeCUD(name,salary,age);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASEURL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+
+        Call<Void> voidCall = employeeAPI.registerEmployee(employeeCUD);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(RegistrationActivity.
+                        this,"You have been successfully registered ",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(RegistrationActivity.this,
+                        "Error :" + t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
