2e75d06fbbe01d63dee5ea1335ffaa67e309cb35
anupant01 <pantaanupa@gmail.com>
2019-05-14 10:53:08 +0545

    Search done

--- app/src/main/java/ClientApi/EmployeeAPI.java
+++ app/src/main/java/ClientApi/EmployeeAPI.java
@@ -5,9 +5,15 @@ import java.util.List;
 import model.Employee;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface EmployeeAPI {
 
+    //to get all the employees
     @GET("employees")
     Call<List<Employee>> getAllEmployee();
+
+    //to get employee on the basis of their id
+    @GET("employee/{empID}")
+    Call<Employee> getEmployeeByID(@Path("empID") int empID);
 }
--- app/src/main/java/com/e/api/SearchEmployeeActivity.java
+++ app/src/main/java/com/e/api/SearchEmployeeActivity.java
@@ -2,9 +2,19 @@ package com.e.api;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import ClientApi.EmployeeAPI;
+import model.Employee;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchEmployeeActivity extends AppCompatActivity {
 
@@ -19,6 +29,51 @@ public class SearchEmployeeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_employee);
 
-        tvData = findViewById()
+        tvData = findViewById(R.id.tvDatta);
+        etSearch = findViewById(R.id.etSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+            }
+        });
+    }
+
+
+    private void loadData(){
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+
+        EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+
+        Call<Employee> listCall = employeeAPI.getEmployeeByID(Integer.parseInt(etSearch.getText().toString()));
+
+        listCall.enqueue(new Callback<Employee>() {
+            @Override
+            public void onResponse(Call<Employee> call, Response<Employee> response) {
+                String content = "";
+                content += "ID : " + response.body().getId() + "\n";
+                content += "Name : " + response.body().getEmployee_name() + "\n";
+                content += "Salary : " + response.body().getEmployee_salary() + "\n";
+                content += "Age : " + response.body().getEmployee_age() + "\n";
+
+
+
+                tvData.append(content);
+
+            }
+
+            @Override
+            public void onFailure(Call<Employee> call, Throwable t) {
+
+                Toast.makeText(SearchEmployeeActivity.this,"Error",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
