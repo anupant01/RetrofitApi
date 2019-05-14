c3c4290c2ee5c1e68829a5f9a914e366bbe4cb0e
anupant01 <pantaanupa@gmail.com>
2019-05-14 09:41:30 +0545

    Initial commit

---
+++ app/src/main/java/com/e/api/DashboardActivity.java
@@ -0,0 +1,30 @@
+package com.e.api;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private Button btnSearch, btnRegister, btnShow, btnUpdate, btnDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        btnShow = findViewById(R.id.btnShow);
+
+
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/e/api/MainActivity.java
@@ -0,0 +1,74 @@
+package com.e.api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ClientApi.EmployeeAPI;
+import model.Employee;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvData;
+
+    private final static String BASE_URL ="http://dummy.restapiexample.com/api/v1/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvData = findViewById(R.id.tvData);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        EmployeeAPI employeeAPI =retrofit.create(EmployeeAPI.class);
+
+        Call<List<Employee>> listCall = employeeAPI.getAllEmployee();
+
+        listCall.enqueue(new Callback<List<Employee>>() {
+            @Override
+            public void onResponse(Call<List<Employee>> call, Response<List<Employee>> response) {
+
+                if(!response.isSuccessful()){
+
+                    tvData.setText("Code : " + response.code());
+                    return;
+                }
+
+                List<Employee> employeeList = response.body();
+
+                for (Employee employee : employeeList)
+                {
+                    String content = "";
+                    content += "ID : " + employee.getId() + "\n";
+                    content += "employee_name : " + employee.getEmployee_name() + "\n";
+                    content += "employee_salary : " + employee.getEmployee_salary() + "\n";
+                    content += "employee_age : " + employee.getEmployee_age() + "\n";
+                    content += "profile_image : " + employee.getProfile_image() + "\n";
+                    content += "-------------------------------" +"\n";
+
+                    tvData.append(content);
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Employee>> call, Throwable t) {
+
+                tvData.setText("Error " +t.getMessage());
+
+            }
+        });
+    }
+}
